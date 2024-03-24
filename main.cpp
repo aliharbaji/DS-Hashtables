78adb2e194d4aea18644087850a11b640e81cd26
Ali <alli.harbaji@gmail.com>
2024-03-24 21:59:53 +0200

    check main for tests

--- main.cpp
+++ main.cpp
@@ -14,13 +14,13 @@ using namespace std;
 int main() {
     cout << "Testing the olympics_t class (uncomment the test you want to see)" << endl;
     // TEST1 (a test for resizing up and down) PASS
-//    testResizingUpAndDown();
+    testResizingUpAndDown();
 
     // TEST2 this is a big test but I tried to keep it commented as much as possible, it works properly up until the stars *** appear
 //    test2();
 
     // TEST3 (a small test for playing matches and removing players) PASS
-    test3();
+//    test3();
 
     return 0;
 }
